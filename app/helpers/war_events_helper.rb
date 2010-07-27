b79a85c6dcac505feacefbf009d225c8e93db5d5
Tom ten Thij <git@tomtenthij.nl>
2010-07-27 03:59:50 +0100

    Layout the show page for a war event a bit better

--- app/helpers/war_events_helper.rb
+++ app/helpers/war_events_helper.rb
@@ -1,2 +1,13 @@
 module WarEventsHelper
+  def wia_kia_table(war_event)
+    s = "<table class='wia_kia'>\n"
+    s << "<tr><th/><th>WIA</th><th>KIA</th></tr>\n"
+    types = %w{civilian host_nation friendly enemy}
+    types.each do |type|
+      s << "<tr><th>#{type.humanize}:</th><td>#{war_event.send(type + "_wia")}</td><td>#{war_event.send(type + "_kia")}</td></tr>\n"
+    end
+    s << "<tr><th colspan='3'>Enemy detained:</th><td>#{war_event.enemy_detained}</td></tr>\n"
+    s << "</table>\n"
+    s
+  end
 end
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,3 @@
+# Tue 27 July, 2010 03:53 BST
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:full] = "%a %d %B, %Y %H:%M %Z"
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:full] = "%a %d %B, %Y %H:%M %Z"
