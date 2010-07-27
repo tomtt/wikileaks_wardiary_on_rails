1ee919bb8aac45355b1281ba7ab61001fed2b949
Tom ten Thij <git@tomtenthij.nl>
2010-07-27 10:05:52 +0100

    Show full date index

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,3 +1,7 @@
 # Tue 27 July, 2010 03:53 BST
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:full] = "%a %d %B, %Y %H:%M %Z"
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:full] = "%a %d %B, %Y %H:%M %Z"
+
+# Tue 2010-07-27 03:53 BST
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:concise] = "%a %Y-%m-%d %H:%M %Z"
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:concise] = "%a %Y-%m-%d %H:%M %Z"
