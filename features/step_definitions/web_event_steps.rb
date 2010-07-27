026db1dae609b4e02b417b61be469bae2efee97c
Tom ten Thij <git@tomtenthij.nl>
2010-07-27 05:53:36 +0100

    Make sure the user ends back up at the same page when clicking back when viewing a war event

--- features/step_definitions/web_event_steps.rb
+++ features/step_definitions/web_event_steps.rb
@@ -1,5 +1,5 @@
 Given /^(\d+) war events exist numbered with the category "([^\"]*)"$/ do |count, category|
   count.to_i.times do  |i|
-    Factory.create(:war_event, :category => category.sub("#n", (i + 1).to_s))
+    Factory.create(:war_event, :category => category.sub("#n", (i + 1).to_s), :date => i.days.ago)
   end
 end
