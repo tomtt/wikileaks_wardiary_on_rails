4d538e9ef9027048a3058a5dbb71d5476b620bbc
Tom ten Thij <git@tomtenthij.nl>
2010-07-28 11:10:36 +0100

    Generated acts_as_taggable_on migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100727181305) do
+ActiveRecord::Schema.define(:version => 20100728100959) do
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "taggable_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
 
   create_table "term_definitions", :force => true do |t|
     t.string   "term",       :null => false
