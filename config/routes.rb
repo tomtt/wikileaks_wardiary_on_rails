cb521a860957afb7d4300db8d0c185b5165686ff
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 11:05:40 +0100

    Route for nesting war events under tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :term_tags
+  map.resources :term_tags do |term_tags|
+    term_tags.resources :war_events
+  end
   map.resources :war_events
   map.root :controller => "war_events", :action => "index"
 end
