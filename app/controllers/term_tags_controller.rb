3e65e7167628b4c9d48d2b4037a26e5fd312794b
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 10:37:08 +0100

    term_tags controller only has index and show methods

--- app/controllers/term_tags_controller.rb
+++ app/controllers/term_tags_controller.rb
@@ -1,5 +1,5 @@
 class TermTagsController < ApplicationController
-  resources_controller_for :term_tags, :class => ActsAsTaggableOn::Tag
+  resources_controller_for :term_tags, :class => ActsAsTaggableOn::Tag, :only => [:index, :show]
 
   def show
     @term_tag = resource_service.find(params[:id])
