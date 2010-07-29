27b25a9fe89d23a2d3575315427008d6fc7539fe
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 05:15:44 +0100

    Always require ruby-debug when available

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
+require "ruby-debug" if Gem.available?("ruby-debug")
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
