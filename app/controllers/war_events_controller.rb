97a03658c47bc324232bfbb8958beea612ffb8ec
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 11:05:12 +0100

    Make war_events controller a RC

--- app/controllers/war_events_controller.rb
+++ app/controllers/war_events_controller.rb
@@ -1,26 +1,18 @@
 class WarEventsController < ApplicationController
-  # GET /war_events
-  # GET /war_events.xml
-  def index
-    @war_events = WarEvent.paginate :page => params[:page], :order => 'date DESC'
+  resources_controller_for :war_events, :only => [:index, :show]
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @war_events }
-    end
-  end
-
-  # GET /war_events/1
-  # GET /war_events/1.xml
   def show
-    @war_event = WarEvent.find_by_report_key(params[:id])
+    @war_event = find_resource
     @term_list = @war_event.term_list
     @tags = @term_list.map { |t| ActsAsTaggableOn::Tag.find_by_name(t) }
     @term_definitions = @term_list.map { |t| TermDefinition.find_by_term(t) }
+  end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @war_event }
-    end
+  def find_resources
+    resource_service.paginate :page => params[:page], :order => 'date DESC'
+  end
+
+  def find_resource
+    WarEvent.find_by_report_key(params[:id])
   end
 end
