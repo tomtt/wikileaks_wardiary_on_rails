a5bff83745c7746fc1bd65f83b4df3e6abed9c8e
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 04:15:29 +0100

    Installed friendly_id gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'will_paginate'
   config.gem 'acts-as-taggable-on'
+  config.gem 'friendly_id'
 
   if %w(development test cucumber).include?(RAILS_ENV)
     config.gem 'rspec', :lib => false
