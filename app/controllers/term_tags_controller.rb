b714b6d3c823e4715a8e37aab2a1f5f7121abefa
Tom ten Thij <git@tomtenthij.nl>
2010-07-29 05:16:51 +0100

    Show tagged events on tag show page

--- app/controllers/term_tags_controller.rb
+++ app/controllers/term_tags_controller.rb
@@ -1,3 +1,12 @@
 class TermTagsController < ApplicationController
   resources_controller_for :term_tags, :class => ActsAsTaggableOn::Tag
+
+  def show
+    @term_tag = resource_service.find(params[:id])
+    @war_events = WarEvent.tagged_with(@term_tag.name).paginate(:page => params[:page], :order => 'date DESC')
+  end
+
+  def find_resources
+    resource_service.all(:order => :name)
+  end
 end
