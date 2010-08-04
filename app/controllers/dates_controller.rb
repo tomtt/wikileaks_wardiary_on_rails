ef9b2e653077ff6f86d6c7389d2ef933cf1546d3
Tom ten Thij <git@tomtenthij.nl>
2010-08-04 08:59:16 +0100

    Render 404 when date could not be parsed

--- app/controllers/dates_controller.rb
+++ app/controllers/dates_controller.rb
@@ -1,6 +1,11 @@
 class DatesController < ApplicationController
   def show
-    @date = Date.parse(params[:id])
+    begin
+      @date = Date.parse(params[:id])
+    rescue ArgumentError
+      render_404 and return
+    end
+
     @page_title = "War events on #{@date.to_s(:concise)}"
 
     @war_events = WarEvent.on_date(@date).paginate(:page => params[:page], :order => 'date ASC')
